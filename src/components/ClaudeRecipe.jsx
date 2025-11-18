94e58e66dea844e7470833de75141f2feae443b5
Elite <kimo123kimo321@gmail.com>
2025-11-18 13:02:02 +0200

    shit

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -11,7 +11,9 @@ import ReactDom from 'react-dom'
 
 export default function ClaudeRecipe(props) {
   return (
-   <section className='suggested-recipe-container'>
+   <section className='suggested-recipe-container'
+   aria-live="polite">
+    <h2>Chef Claude Recommends:</h2>
     <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
